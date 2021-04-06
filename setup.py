53a01d97537c5b50ecd886a1785657c88ed70a33
nirajparsai <niraj.parsai@gmail.com>
2021-04-06 20:43:03 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='pypackage',
+version='3.8',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='author@email.com',
+license='no',
+packages=['pypackage'],
+zip_safe=False)
