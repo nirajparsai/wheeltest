d557ca331f6bab68841803662c3f312f29b1e4a5
nirajparsai <niraj.parsai@gmail.com>
2021-04-08 20:27:41 +0530

    Update test.py

--- MyApp/pypackage/test.py
+++ MyApp/pypackage/test.py
@@ -1,4 +1,12 @@
-from pypackage.functions import *
+def sum(x,y):
+    return x+y
+
+def average(x,y):
+    return (x+y)/2
+
+def power(x,y):
+    return x**y
+  
 print(sum(2,5))
 print(power(2,5))
 print(average(2,5))
